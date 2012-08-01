4481f284251930b27ee8a81cc628a0915eafd521
Martin Cousin <martin.cousin@gmx.de>
2012-08-01 19:25:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tippspiel::Application.config.secret_token = '8aaf7fdc3a12b568f4ac1fd893682cbb462ce0d4ecdbdab961ee80537eabf425f7f4525be99fe936ea6d328897b19171382069c32da711dd3a13448e9f7307e4'
