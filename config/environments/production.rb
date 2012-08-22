809d0881198fb2cf438659e06fcd0854b706883b
Martin Cousin <martin.cousin@gmx.de>
2012-08-22 18:38:16 +0200

    heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Tippspiel::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -24,7 +24,6 @@ Tippspiel::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = nil # for heroku?
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
